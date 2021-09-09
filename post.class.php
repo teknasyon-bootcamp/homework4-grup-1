2faae0a73df941cc82e970e5dd16cab71cc142ab
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-09-10 01:58:17 +0300

    Add Post model

--- post.class.php
+++ post.class.php
@@ -1,1 +1,18 @@
+<?php
+
+
+require_once 'db.class.php';
+
+
+class Post extends DB{
+
+    
+    public int $id;
+    public string $title = '';
+    public string $content = '';
+
+    public function __construct(
+    ){}
+
+}
 
